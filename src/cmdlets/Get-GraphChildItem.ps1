e62e43e87792b175843bfe5c90290b97ea6f2ba0
Adam Edwards <adamedx@gmail.com>
2018-09-05 06:22:12 -0700

    Add -search option to Get-GraphChildItem

--- src/cmdlets/Get-GraphChildItem.ps1
+++ src/cmdlets/Get-GraphChildItem.ps1
@@ -26,6 +26,8 @@ function Get-GraphChildItem {
 
         [String] $ODataFilter = $null,
 
+        [String] $Search = $null,
+
         [String[]] $Select = $null,
 
         [String[]] $Expand = $null,
@@ -107,6 +109,7 @@ function Get-GraphChildItem {
         RelativeUri=$ItemRelativeUri[0]
         Query = $Query
         ODataFilter = $ODataFilter
+        Search = $Search
         Select = $Select
         Expand = $Expand
         OrderBy = $OrderBy
