f7e5684f00eae5d2d07cc8138afa91c73898e1a4
Adam Edwards <adamedx@gmail.com>
2018-09-21 22:16:13 -0700

    Add warning messages at module load to point people to AutoGraphPS

--- src/graph.ps1
+++ src/graph.ps1
@@ -15,7 +15,17 @@
 # This breaks without the import line below, but I don't think it should.
 # It appears to be related to the scriptclass module and whether it has
 # already been loaded as a nested module of a module being imported here
-import-module poshgraph-sdk
+
+write-warning "***DEPRECATION!!!***"
+write-warning "PoshGraph has been renamed to *AutoGraphPS*."
+write-warning "The module package for PoshGraph will no longer be updated!"
+write-warning "Visit the link below for more details regarding the rename:"
+write-warning "`n`n         https://github.com/adamedx/autographps/blob/poshgraph_rename/README.md#this-module-has-been-renamed`n`n"
+write-warning "Please replace this module with AutoGraphPS to get the latest"
+write-warning "version of all your favorite PoshGraph cmdlets:"
+write-warning "`n`n         Uninstall-Module poshgraph -Scope CurrentUser`n         Install-Module AutoGraphPS -Scope CurrentUser"
+
+import-module autographps-sdk
 
 . (import-script cmdlets)
 . (import-script aliases)
