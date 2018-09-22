90c46e2d2df5cbac0a7224ea3cd74ddff787958a
Adam Edwards <adamedx@gmail.com>
2018-09-22 07:26:03 -0700

    Use deprecation from poshgraph-sdk

--- src/graph.ps1
+++ src/graph.ps1
@@ -16,16 +16,7 @@
 # It appears to be related to the scriptclass module and whether it has
 # already been loaded as a nested module of a module being imported here
 
-write-warning "***DEPRECATION!!!***"
-write-warning "PoshGraph has been renamed to *AutoGraphPS*."
-write-warning "The module package for PoshGraph will no longer be updated!"
-write-warning "Visit the link below for more details regarding the rename:"
-write-warning "`n`n         https://github.com/adamedx/autographps/blob/poshgraph/README.md#this-module-has-been-renamed`n`n"
-write-warning "Please replace this module with AutoGraphPS to get the latest"
-write-warning "version of all your favorite PoshGraph cmdlets:"
-write-warning "`n`n         Uninstall-Module poshgraph -Scope CurrentUser`n         Install-Module AutoGraphPS -Scope CurrentUser"
-
-import-module autographps-sdk
+import-module poshgraph-sdk
 
 . (import-script cmdlets)
 . (import-script aliases)
