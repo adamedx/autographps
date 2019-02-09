d5a353bda706894557778c251a348aa7e659255b
Adam Edwards <adamedx@gmail.com>
2019-02-09 13:37:31 -0800

    More linux compat fixes

--- build/build-package.ps1
+++ build/build-package.ps1
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+[cmdletbinding()]
 param($targetDirectory = $null, [switch] $noclean, [switch] $DownloadDependencies)
 
 . "$psscriptroot/common-build-functions.ps1"
